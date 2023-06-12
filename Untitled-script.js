6a075131ba8b242b4825366ec582913efc625445
Tulza <40633228+tulza@users.noreply.github.com>
2023-06-12 16:09:47 +1200

    fake loading screen implemented

---
+++ Untitled-script.js
@@ -0,0 +1,27 @@
+// let progressCircle = document.querySelector(".progress");
+// let radius = progressCircle.r.baseVal.value;
+// let timeValue = 0
+// const duration = 200
+// const maxValue = 100;
+// const increment = maxValue / duration;
+
+// let circumference = radius * 2 * Math.PI;
+// progressCircle.style.strokeDasharray = circumference;
+
+// //0-100
+// setProgress(timeValue);
+
+// function setProgress(percent){
+//     progressCircle.style.strokeDashoffset = circumference - (percent/100) * circumference;
+// }
+
+// updateTime()
+// function updateTime() {
+//     if (timeValue < maxValue) {
+//       timeValue += increment;
+//       // Do something with the updated timeValue, such as updating the progress circle
+//       console.log(timeValue);
+//       setTimeout(updateTime, 1); // Update the value every millisecond
+//     }
+//   }
+
