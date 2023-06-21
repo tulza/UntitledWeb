8a193805b7df3890134823a758435ca43b668a6e
Tulza <40633228+tulza@users.noreply.github.com>
2023-06-22 03:10:36 +1200

    Centered header text - +some javascript mess around

--- Untitled-script.js
+++ Untitled-script.js
@@ -1,32 +1,41 @@
-// let progressCircle = document.querySelector(".progress");
-// let radius = progressCircle.r.baseVal.value;
-// let timeValue = 0
-// const duration = 200
-// const maxValue = 100;
-// const increment = maxValue / duration;
-
-// let circumference = radius * 2 * Math.PI;
-// progressCircle.style.strokeDasharray = circumference;
-
-// //0-100
-// setProgress(timeValue);
-
-// function setProgress(percent){
-//     progressCircle.style.strokeDashoffset = circumference - (percent/100) * circumference;
-// }
-
-// updateTime()
-// function updateTime() {
-//     if (timeValue < maxValue) {
-//       timeValue += increment;
-//       // Do something with the updated timeValue, such as updating the progress circle
-//       console.log(timeValue);
-//       setTimeout(updateTime, 1); // Update the value every millisecond
-//     }
-//   }
-
-
 setTimeout(() => {
     const elem = document.getElementById("preloader");
     elem.parentNode.removeChild(elem);
 }, 2100);
+
+console.log('hi');
+
+let luckyNum = 7;
+console.log(luckyNum);
+
+luckyNum = new Object();
+console.log(luckyNum);
+
+luckyNum = {
+    luckType: 'pogas',
+    number: 7,
+    creation: '21/06/23',
+};
+
+console.log(luckyNum)
+
+function add(a, b){
+    return a+b
+};
+console.log(this)
+wtfIsThis();
+function wtfIsThis(){
+    console.log(this);
+};
+
+const person = {
+    wtfIsThis: () => {
+        console.log(this);
+    }
+}
+
+console.log(person)
+
+function CLICKED(){
+    alert('random button')
+}
